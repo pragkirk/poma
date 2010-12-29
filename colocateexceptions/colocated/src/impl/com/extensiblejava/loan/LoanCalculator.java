ea8c752dde439ad0ac7bbede7ae14f6d78e93587
Kirk Knoernschild <pragkirk@kirkk.com>
2010-12-29 09:19:11 -0600

    Added exception to method definition.

--- colocateexceptions/colocated/src/impl/com/extensiblejava/loan/LoanCalculator.java
+++ colocateexceptions/colocated/src/impl/com/extensiblejava/loan/LoanCalculator.java
@@ -3,7 +3,7 @@ package com.extensiblejava.loan;
 import java.math.*;
 
 public interface LoanCalculator {
-	public PaymentSchedule calculatePaymentSchedule(BigDecimal presentValue, BigDecimal rate, int term);
+	public PaymentSchedule calculatePaymentSchedule(BigDecimal presentValue, BigDecimal rate, int term) throws CalculationException;
 	public BigDecimal getCumulativeInterest();
 	public BigDecimal getCumulativePrincipal();
 
--- colocateexceptions/osgi/src/impl/com/extensiblejava/loan/LoanCalculator.java
+++ colocateexceptions/osgi/src/impl/com/extensiblejava/loan/LoanCalculator.java
@@ -3,7 +3,7 @@ package com.extensiblejava.loan;
 import java.math.*;
 
 public interface LoanCalculator {
-	public Loan calculateLoan(BigDecimal presentValue, BigDecimal rate, int term);
+	public Loan calculateLoan(BigDecimal presentValue, BigDecimal rate, int term) throws CalculationException;
 	/*public BigDecimal getCumulativeInterest();
 	public BigDecimal getCumulativePrincipal();*/
 
